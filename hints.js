f790fae61851db147f81f84bacceb0f97af02b5e
gsmaverick <gavin.schulz@gmail.com>
2012-03-29 16:57:18 -0400

    Finished package.json description

--- hints.js
+++ hints.js
@@ -1,4 +1,17 @@
 var hints = {
-  "name": "<p>It may happen that a city has multiple GRAF datasets for different things so it's best to give each data set a name that is descriptive of what data is contains.</p>",
-  "version": ""
+  "name":         "<p>It may happen that a city has multiple GRAF datasets for different things so it's best to give each data set a name that is descriptive of what data is contains.</p>",
+  "license":      "<p>License that the data set is released under.  This can be a string or a link to the text of the license.  A file called LICENSE should be included with every GRAF "
+                  + "package that contains all licensing information that governs the usage and distribution of the data set.</p>",
+  "created_at":   "<p>Date and time this specific revision of the data set was created.</p>",
+  "version":      "<p>Defines the version of the GRAF spec this data set uses.</p>",
+  "municipality": "<p>Contains extra information about the municipality that this data set covers.</p>",
+  "city":         "<p></p>",
+  "province":     "<p></p>",
+  "country":      "<p></p>",
+  "website":      "<p>This can be the URL of the city's home page or a link to their open data page.</p>",
+  "schema":       "<p>Since GRAF allows you to flexibly add arbitrary fields to your data this array defines any extra fields that have been added to this data set.</p>",
+  "table":        "<p>One which `table` was the field added.</p>",
+  "field":        "<p>The name of the field that was added.</p>",
+  "description":  "<p>Lets the users of data set know what the purpose of the added field is.</p>",
+  "type":         "<p>This can be empty if the type of the field doesn't matter otherwise is should be one of the JSON data types.</p>"
 };
