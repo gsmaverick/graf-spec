1121d793965088c5722b71c1ac00b838ce1a66c5
gsmaverick <gavin.schulz@gmail.com>
2012-03-29 15:44:26 -0400

    first pages commit

---
+++ hints.js
@@ -0,0 +1,4 @@
+var hints = {
+  "name": "<p>It may happen that a city has multiple GRAF datasets for different things so it's best to give each data set a name that is descriptive of what data is contains.</p>",
+  "version": ""
+};
